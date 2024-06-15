6376de8e67e7088e3da0a6359ee3ca85dde24d29
OmarThierno <thiernomarndiaye76@gmail.com>
2024-06-15 22:03:24 +0200

    feat: add bootstrap and fontawesome dependencies

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '~resources': '/resources/'
+            '~resources': '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~fontawesome': path.resolve(__dirname, 'node_models/fortawesome')
         }
     },
 });
